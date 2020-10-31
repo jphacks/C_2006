88241a64532317d440a8d59af7737996f490a0b4
Shoma <sho1228.k@gmail.com>
2020-10-31 13:12:56 +0900

    Fix typo

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -13,7 +13,7 @@
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="personCircleOutlineS" />
+          <ion-icon :icon="personCircleOutline" />
           <ion-label>Mypage</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
