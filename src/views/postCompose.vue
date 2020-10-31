95d30acc7e95cd34788a05de9af71d8ec4bc69b4
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-10-31 14:38:03 +0900

    Update: send sample post data to firebase

---
+++ src/views/postCompose.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Compose</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Componse</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <button @click="sendPost()">send!</button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import firebase from 'firebase';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+export default  {
+  name: 'Tab1',
+  
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data() {
+    return {
+      newPost: {
+        
+      }
+    }
+  },
+  methods: {
+    sendPost() {
+      firebase.database().ref('posts').push({
+        title: "fuga",
+        text: "fugafuga"
+      });
+      console.log('send!')
+    }
+  }
+}
+</script>
