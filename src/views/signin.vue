1c1e50a3a2035c3537a4a1f5b3b68393c90fa513
Shoma <sho1228.k@gmail.com>
2020-10-31 22:27:27 +0900

    Update signin-view

---
+++ src/views/signin.vue
@@ -0,0 +1,81 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Signin</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Signin</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div class="wrapper">
+        <img src="../../public/assets/logo.svg" alt="logo" class="img">
+        <div class="forms">
+          <ion-list>
+            <ion-item>
+              <ion-label position="stacked">email</ion-label>
+              <ion-input placeholder="email" type="email"></ion-input>
+            </ion-item>
+
+            <ion-item>
+              <ion-label position="stacked">password</ion-label>
+              <ion-input placeholder="password" type="password"></ion-input>
+            </ion-item>
+          </ion-list>
+
+          <ion-button class="update">
+            <ion-icon slot="start" :icon="logInOutline"></ion-icon>
+            Signin
+          </ion-button>
+        </div>
+
+    
+        <ion-button class="logout" href="/signup">
+          <ion-icon slot="start" :icon="personAddOutline"></ion-icon>
+          go to Signup page
+        </ion-button>
+      </div>
+      
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonLabel, IonInput, IonItem, IonList , IonIcon} from '@ionic/vue';
+import { personAddOutline, logInOutline } from 'ionicons/icons';
+
+export default  {
+  name: 'Tab2',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonLabel, IonInput, IonItem, IonList, IonIcon },
+  setup() {
+    return {
+      personAddOutline,
+      logInOutline
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper{
+  text-align: center;
+}
+.img{
+    width: 80%;
+    margin: auto;
+}
+.forms{
+  margin: 0 5%;
+}
+.update{
+  margin-top: 30px;
+}
+.logout{
+  margin-top: 50px;
+}
+</style>
