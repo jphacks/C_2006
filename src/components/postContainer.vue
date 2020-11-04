155a41a325bfb7574c8893689956eca0d3781637
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-11-04 20:10:37 +0900

    Fix: search -> postDetail

--- src/components/postContainer.vue
+++ src/components/postContainer.vue
@@ -2,7 +2,7 @@
   <ion-grid>
     <ion-row>
       <ion-col v-for="post in posts"  :key="post.id" size="4">
-        <img :src="post.imageUrl" alt="picture" @click="$emit('postid',post.id)">
+        <img :src="post.imageUrl" alt="picture" @click="$emit('postid',post.key)">
       </ion-col>
     </ion-row>
   </ion-grid>
