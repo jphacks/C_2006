0c818c97b3a0ddb691a53a593733b83bb77275ae
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-11-05 23:16:09 +0900

    Add: get stocked posts key

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -13,6 +13,7 @@
       </ion-header>
     
       <post-container @postid="toDetailView"/>
+      {{ posts }}
     </ion-content>
   </ion-page>
 </template>
@@ -22,6 +23,8 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue
 import { useRouter } from 'vue-router';
 import postContainer from '@/components/postContainer.vue';
 
+import firebase from 'firebase';
+
 export default  {
   name: 'Tab1',
   components: { postContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
@@ -30,10 +33,32 @@ export default  {
       router: useRouter()
     }
   },
+  data() {
+    return {
+      posts: [] as any,
+    }
+  },
   methods: {
     toDetailView(id: string) {
       (this as any).router.push(`/post/${id}`);
-    }
+    },
+    async storeInPosts(data: object) {
+      (this as any).posts = Object.entries(data).map(([key, value]) => ({
+        key: key,
+        post: value.key
+      }));
+    },
+  },
+  async created() {
+    const uid = firebase.auth().currentUser?.uid;
+    await firebase.database().ref(`stocks/${uid}`)
+      .orderByChild('key')
+      .limitToLast(10)
+      .once('value')
+      .then(((snapshot) => {
+        console.log(snapshot.val());
+        (this as any).storeInPosts(snapshot.val());
+      }));
   }
 }
 </script>
